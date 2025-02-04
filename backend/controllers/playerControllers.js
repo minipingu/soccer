0adea4c774724eaa88da6f34287252faa012424f
minipingu <ranmaniax@gmail.com>
2025-02-05 05:07:15 +0700

    GET all and by id

--- backend/controllers/playerControllers.js
+++ backend/controllers/playerControllers.js
@@ -13,3 +13,21 @@ export const addNewPlayer = async (req, res) => {
 		res.send(err)
 	}
 }
+
+export const getAllPlayers = async (req, res) => {
+	try {
+		const players = await Player.find()
+		res.json(players)
+	} catch (err) {
+		res.send(err)
+	}
+}
+
+export const getPlayerWithId = async (req, res) => {
+	try {
+		const player = await Player.findById(req.params.PlayerId)
+		res.json(player)
+	} catch (err) {
+		res.send(err)
+	}
+}
--- backend/routes/soccerRoutes.js
+++ backend/routes/soccerRoutes.js
@@ -1,7 +1,15 @@
-import { addNewPlayer } from '../controllers/playerControllers'
+import {
+	addNewPlayer,
+	getAllPlayers,
+	getPlayerWithId,
+} from '../controllers/playerControllers'
 
 const routes = (app) => {
-	app.route('/players').post(addNewPlayer)
+	app.route('/players')
+		.get(getAllPlayers) //GET endpoint
+		.post(addNewPlayer) // POST endpoint
+
+	app.route('/player/:PlayerId').get(getPlayerWithId) // GET specific player
 }
 
 export default routes
